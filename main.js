14c03b83e08be8770c2a8d7dc824faf64f0838b6
wwmin <wwei.min@163.com>
2017-01-18 14:01:08 +0800

    调整逻辑,硬座和硬卧用用同一个判断

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ var nodemailer = require('nodemailer');
 var schedule = require('node-schedule');
 var options = {
   hostname: 'kyfw.12306.cn',
-  path: '/otn/leftTicket/queryA?leftTicketDTO.train_date=2017-01-21&leftTicketDTO.from_station=SHH&leftTicketDTO.to_station=SRG&purpose_codes=ADULT',
-  // rejectUnauthorized: false  // 忽略安全警告
-  ca: [ca]
+   path: '/otn/leftTicket/queryA?leftTicketDTO.train_date=2017-01-28&leftTicketDTO.from_station=SHH&leftTicketDTO.to_station=SRG&purpose_codes=ADULT',
+    // rejectUnauthorized: false  // 忽略安全警告
+    ca:[ca]
 };
+var yz_temp = '',
+  yw_temp = '';
 
 function queryTickets() {
   var req = https.get(options, function (res) {
@@ -24,7 +26,6 @@ function queryTickets() {
     });
     res.on('data', function (buff) {
       data += buff;
-      console.log(data);
     });
     res.on('end', function () {
       var jsonData = JSON.parse(data).data;
@@ -33,14 +34,20 @@ function queryTickets() {
         if (cur.queryLeftNewDTO.station_train_code == 'K1209') {
           var yz = cur.queryLeftNewDTO.yz_num;
           var yw = cur.queryLeftNewDTO.yw_num;
-          if (!isNaN(yz)) {
-            console.log(yz);
+          var trainNum = cur.queryLeftNewDTO.station_train_code;
+          console.log('硬座:', yz);
+          console.log('硬卧:', yw);
+          if (yz != '无' && yz != '--' || yw != '无' && yw != '--') {
+            if (yw_temp == yw && yz_temp == yz) {
+              console.log('状态没改变,不重复发邮件.');
+              return;
+            }
             var mailOptions = {
               from: 'wwei.min@163.com', //发件地址
               to: 'wwei.min@163.com', //收件列表
-              subject: '有硬座票啦,剩余' + yz + '张', //标题
+              subject: '有硬座票啦,剩余' + yz + '张,硬卧:' + yw + '张', //标题
               //text和html两者只支持一种
-              text: '有硬座啦,剩余' + yz + '张' //内容
+              text: trainNum + '有硬座啦,剩余' + yz + '张,硬卧:' + yw + '张' //内容
               //html:'<b>Hello world.</b>'//html内容
             };
             //send mail with define transport object
@@ -49,30 +56,11 @@ function queryTickets() {
                 return console.log(error);
               }
               console.log('Message sent:' + info.response);
+              yw_temp = yw;
+              yz_temp = yz;
             })
           } else {
-              console.log('硬座无票');
-              log
-          }
-          if (!isNaN(yw)) {
-            console.log(yw);
-            yw = yw || '';
-            var mailOptions = {
-              from: 'wwei.min@163.com', //发件地址
-              to: 'wwei.min@163.com', //收件列表
-              subject: '有硬座票啦,剩余' + yz + '张', //标题
-              //text和html两者只支持一种
-              text: '有硬座啦,剩余' + yz + '张' //内容
-              //html:'<b>Hello world.</b>'//html内容
-            };
-            transporter.sendMail(mailOptions, function (error, info) {
-              if (error) {
-                return console.log(error);
-              }
-              console.log('Message sent:' + info.response);
-            });
-          } else {
-            console.log('硬卧无票');
+            console.log('硬座/硬卧无票');
           }
           break;
         }
@@ -85,7 +73,7 @@ function queryTickets() {
 }
 
 var rule = new schedule.RecurrenceRule();
-rule.second = [0,15,30,45];
+rule.second = [0];
 schedule.scheduleJob(rule, function () {
   queryTickets();
   console.log('scheduleCronstyle:' + new Date());
